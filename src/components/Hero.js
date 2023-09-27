27e4a43daf0f4c5bd94b500feca2880290c03d36
Vishnu Sharma <vs510514@gmail.com>
2023-09-28 03:14:05 +0530

    initial commit

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -7,7 +7,7 @@ const Hero = () => {
     <div class="container">
         <div class="hero" id="Home">
             <div class="left flex column">
-                <h1 style={{fontSize:"4.5em;"}}>Monitoring Your Daily Life</h1>
+                <h1 >Monitoring Your Daily Life</h1>
                 <div class="sub-text">We passion to work hard and deliver excellent results. It could solve the needs of
                     your customers and develop innovation.</div>
                 <div class="btn">
